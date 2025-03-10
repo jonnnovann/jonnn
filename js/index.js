17a455be689f70a84b27fd8d4c3c3b3c9aae7cd2
jonnnovann <fernandoyudon@gmail.com>
2025-03-11 00:03:13 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Gua punya sesuatu nih'.split('')
+const text = 'Halo Maulia Rachmah Hakim'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
